7946fb58f885a24f9873139527aca8cb8aa6a039
fundowakin <fundowakin@gmail.com>
2022-01-29 16:15:48 +0200

    Services UOW created

---
+++ BLL/Services/ResultService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using BLL.DTOs;
+
+namespace BLL.Services
+{
+    public class ResultService : IService<ResultDTO>
+    {
+        public IEnumerable<ResultDTO> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
