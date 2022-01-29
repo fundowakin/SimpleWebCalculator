44c7406c35f348c59b8c0134d3d08371728cbb25
fundowakin <fundowakin@gmail.com>
2022-01-29 16:42:29 +0200

    Services update

--- BLL/Services/ExpressionService.cs
+++ BLL/Services/ExpressionService.cs
@@ -1,7 +1,27 @@
-﻿namespace BLL.Services
+﻿using System.Collections.Generic;
+using AutoMapper;
+using BLL.DTOs;
+using DAL.Models;
+using DAL.UOW;
+
+namespace BLL.Services
 {
-    public class ExpressionService
+    public class ExpressionService : IService<ExpressionDTO>
     {
-        
+        private UnitOfWork _unitOfWork;
+
+        public ExpressionService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<ExpressionDTO> GetAll()
+        {
+            var result = _unitOfWork.ExpressionRepository.GetAll();
+            var config = new MapperConfiguration(cfg 
+                => cfg.CreateMap<Expression, ExpressionDTO>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<IEnumerable<ExpressionDTO>>(result);
+        }
     }
 }
--- BLL/Services/ResultService.cs
+++ BLL/Services/ResultService.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoMapper;
 using BLL.DTOs;
+using DAL.Models;
+using DAL.UOW;
 
 namespace BLL.Services
 {
     public class ResultService : IService<ResultDTO>
     {
+        private UnitOfWork _unitOfWork;
+
+        public ResultService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IEnumerable<ResultDTO> GetAll()
         {
-            throw new System.NotImplementedException();
+            var result = _unitOfWork.ResultRepository.GetAll();
+            var config = new MapperConfiguration(cfg 
+                => cfg.CreateMap<Result, ResultDTO>());
+            var mapper = config.CreateMapper();
+            return mapper.Map<IEnumerable<ResultDTO>>(result);
         }
     }
 }
